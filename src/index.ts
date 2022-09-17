bc5e4927b064fcee7b726b82fd4e4c8a8a45964e
Samuel MBote <36938470+SamuelMBote@users.noreply.github.com>
2022-09-16 23:40:11 -0300

    Aprendendo TypeScript, projeto Calculador, configuracao do ambiente com webpack

---
+++ src/index.ts
@@ -0,0 +1,7 @@
+// Import our custom CSS
+import './scss/style.scss';
+// Import all of Bootstrap's JS
+import * as bootstrap from 'bootstrap';
+import CalculadoraControle from './calculadora/CalculadoraControle';
+
+new CalculadoraControle();
