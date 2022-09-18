0973c418793a7ebd408876c823655f9f315e8805
Samuel MBote <36938470+SamuelMBote@users.noreply.github.com>
2022-09-17 23:15:54 -0300

    Exibindo mumeros na tela

--- src/calculadora/CalculadoraControle.ts
+++ src/calculadora/CalculadoraControle.ts
@@ -3,7 +3,43 @@ import Tela from './Tela';
 export default class CalculadoraControle {
   constructor(private tela = new Tela()) {
     new DataHora();
-    this.tela.conteudo = '123.45689101';
-    console.log(tela.conteudo);
+    this.eventoBotoes();
+  }
+  eventoBotoes(): void {
+    document.querySelectorAll('#teclado').forEach((elemento) => {
+      elemento.addEventListener('click', (evento: Event) => {
+        const target = evento.target as HTMLButtonElement;
+        switch (target.id) {
+          case 'um':
+          case 'dois':
+          case 'três':
+          case 'quatro':
+          case 'cinco':
+          case 'seis':
+          case 'sete':
+          case 'oito':
+          case 'nove':
+            this.adicionarNumero(Number(target.dataset.valor));
+            break;
+          case 'adicao':
+          case 'substracao':
+          case 'divisao':
+          case 'multiplicacao':
+            break;
+          case 'ponto':
+            break;
+          case 'limpar':
+            break;
+          case 'desfazer':
+            break;
+          case 'porcentagem':
+            break;
+          case 'igual':
+        }
+      });
+    });
+  }
+  adicionarNumero(numero: number): void {
+    this.tela.conteudo = numero.toString();
   }
 }
